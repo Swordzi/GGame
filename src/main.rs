fdf2e105ced1a06a5799c88275b0f5248c5bca06
Swordzi <github@sword.mozmail.com>
2022-06-19 22:05:41 +0300

    Added the ability to see the number

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,18 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Please input your guess.");
-
     let u8secret_number: u8 = gen::generator();
     let mut guessed: bool = false;
+    let mut bypass: String = String::new();
+
+
+    println!("Do you want to see the secret number? (y/n)");
+    io::stdin()
+        .read_line(&mut bypass).expect("TODO: panic message");
+    if bypass.trim() == "y" {
+        println!("The secret number is {}", u8secret_number);
+    }
+    println!("Please input your guess.");
     while guessed == false {
         {
             println!("Please input your guess.");
